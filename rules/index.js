eaf9ab6de06ea579009b81bd17666653b37e14a7
Sasaki Yuto <reggaepunch@gmail.com>
2017-07-17 12:06:43 +0700

    WIP: add

--- rules/index.js
+++ rules/index.js
@@ -1,3 +1,4 @@
 module.exports = {
-  'no-duplicate-selector': require('./no-duplicate-selectors')
+  'no-duplicate-selector': require('./no-duplicate-selectors'),
+  'file-name-match': require('./file-name-match')
 };
